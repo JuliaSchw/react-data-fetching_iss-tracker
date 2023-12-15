d70755f497836e6e2ec7a17fa24ad08a1ebaec46
Julia Schwerdtfeger <js.schwerdt@gmail.com>
2023-12-15 12:08:55 +0100

    solve challenge

--- components/ISSTracker/index.js
+++ components/ISSTracker/index.js
@@ -1,44 +1,62 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import useSWR, { mutate } from "swr";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
-export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
+// FETCHING DATA START
 
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
-    }
+const fetcher = async (url) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch ISS coordinates");
   }
+  const data = await response.json();
+  return { longitude: data.longitude, latitude: data.latitude };
+};
+
+// FETCHING DATA END
+
+export default function ISSTracker() {
+  const { data, error } = useSWR(URL, fetcher);
+
+  // INTERVAL START
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+    const timer = setInterval(() => {}, 5000);
+    mutate(URL);
 
     return () => {
       clearInterval(timer);
     };
   }, []);
 
+  // INTERVAL END
+
+  // ERROR HANDLING START
+
+  if (error) {
+    console.error(error);
+  }
+
+  // ERROR HANDLING END
+
+  // REFRESH BUTTON START (don't forget to import mutate from SWR)
+
+  const handleRefresh = () => {
+    mutate(URL);
+  };
+
+  // REFRESH BUTTON END
+
   return (
     <main>
-      <Map longitude={coords.longitude} latitude={coords.latitude} />
+      <Map longitude={data?.longitude || 0} latitude={data?.latitude || 0} />
       <Controls
-        longitude={coords.longitude}
-        latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        longitude={data?.longitude || 0}
+        latitude={data?.latitude || 0}
+        onRefresh={handleRefresh}
       />
     </main>
   );
